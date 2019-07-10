4cdc97b6da82f2a1b449151dd09e2be908eadf26
astersetu99 <52713286+astersetu99@users.noreply.github.com>
2019-07-10 13:22:20 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,35 +11,44 @@ bot.on('message', message =>{
 
     if(message.author.id != "598413507595075617" && message.channel.id === "595636170025730048"){
         if(msg.startsWith('.', 0)){
-            if(msg === prefix + "c1" && message.channel.id === "595636170025730048"){
+            if(msg === prefix + "mk1" && message.channel.id === "595636170025730048"){
                 message.channel.send('========QUESTION NO. 1========') // Sends a message to the channel, with the content
             }
-                if(msg === prefix + "c2" && message.channel.id === "595636170025730048"){
+                if(msg === prefix + "mk2" && message.channel.id === "595636170025730048"){
                 message.channel.send('========QUESTION NO. 2========') // Sends a message to the channel, with the content
                 }
-             if(msg === prefix + "c3" && message.channel.id === "595636170025730048"){
+             if(msg === prefix + "mk3" && message.channel.id === "595636170025730048"){
                 message.channel.send('========QUESTION NO. 3========') // Sends a message to the channel, with the content
              }
-            if(msg === prefix + "c4" && message.channel.id === "595636170025730048"){
+            if(msg === prefix + "mk4" && message.channel.id === "595636170025730048"){
                 message.channel.send('========QUESTION NO. 4========') // Sends a message to the channel, with the content
             }
-            if(msg === prefix + "c5" && message.channel.id === "595636170025730048"){
+            if(msg === prefix + "mk5" && message.channel.id === "595636170025730048"){
                 message.channel.send('========QUESTION NO. 5========') // Sends a message to the channel, with the content
             }
-            if(msg === prefix + "c6" && message.channel.id === "595636170025730048"){
+            if(msg === prefix + "mk6" && message.channel.id === "595636170025730048"){
                 message.channel.send('========QUESTION NO. 6========') // Sends a message to the channel, with the content
             }
-                if(msg === prefix + "c7" && message.channel.id === "595636170025730048"){
+                if(msg === prefix + "mk7" && message.channel.id === "595636170025730048"){
                 message.channel.send('========QUESTION NO. 7========') // Sends a message to the channel, with the content
             }
-                if(msg === prefix + "c8" && message.channel.id === "595636170025730048"){
+                if(msg === prefix + "mk8" && message.channel.id === "595636170025730048"){
                 message.channel.send('========QUESTION NO. 8========') // Sends a message to the channel, with the content
             }
-                if(msg === prefix + "c9" && message.channel.id === "595636170025730048"){
+                if(msg === prefix + "mk9" && message.channel.id === "595636170025730048"){
                 message.channel.send('========QUESTION NO. 9========') // Sends a message to the channel, with the content
             }
-                if(msg === prefix + "c10" && message.channel.id === "595636170025730048"){
+                if(msg === prefix + "mk10" && message.channel.id === "595636170025730048"){
                 message.channel.send('========QUESTION NO. 10========') // Sends a message to the channel, with the content
+            }
+              if(msg === prefix + "mk11" && message.channel.id === "595636170025730048"){
+                message.channel.send('========QUESTION NO. 11========') // Sends a message to the channel, with the content
+            }
+              if(msg === prefix + "mk12" && message.channel.id === "595636170025730048"){
+                message.channel.send('========QUESTION NO. 12========') // Sends a message to the channel, with the content
+            }
+              if(msg === prefix + "mk13" && message.channel.id === "595636170025730048"){
+                message.channel.send('========QUESTION NO. 13========') // Sends a message to the channel, with the content
             }
         
            
